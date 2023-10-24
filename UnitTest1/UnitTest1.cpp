7e966607afea60bc1cd1a9553f2d0b05c7825858
SashaBohdanov <oleksandr.bohdanov.ik.2023@lpnu.ua>
2023-10-25 00:09:53 +0300

    first master commit

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Pr5.1/Pr5.1.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+    TEST_CLASS(UnitTest1)
+    {
+    public:
+
+        TEST_METHOD(TestMethod1)
+        {
+            double c;
+            c = (k(1 + p * p, 1 - q * q) - (1 + p * q) * (1 + p * q)) / (1 + k(p * q, 1));
+            Assert::AreEqual(c, 0.997796, 0.00001);
+
+        }
+    };
+}
